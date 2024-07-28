3df9ae7d4c347ca2db4e68ac6710affc2be84c05
NoctuaCode <noctuaapps@gmail.com>
2024-07-29 10:06:31 +1100

    fix(stow): review stow configuration

--- .config/sketchybar/colors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export WHITE=0xffffffff
-export GREEN=0xff1dfca1
-
-# -- Catppuccin Scheme --
-export BAR_COLOR=0xff1e1e2e
-export ITEM_BG_COLOR=0xff89b4fa
-export ACCENT_COLOR=0xfff5c2e7
