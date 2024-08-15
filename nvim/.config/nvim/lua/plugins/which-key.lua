a60607a87a8e0766cb4ebecc186d02aeed2e47ec
NoctuaCode <noctuaapps@gmail.com>
2024-08-16 09:01:42 +1100

    feat(Neovim): Add git group in whichkey

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -10,6 +10,7 @@ return {
         { '<leader>c', group = '[C]ode' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>g', group = '[G]it' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
     end,
