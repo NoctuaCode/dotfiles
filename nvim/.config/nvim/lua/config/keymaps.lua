bfbda246a3aa891272a47bfbacb8f822d917affe
NoctuaCode <noctuaapps@gmail.com>
2024-08-26 07:55:50 +1100

    feat: Update neovim config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Do things without affecting the registers
+keymap.set("n", "x", '"_x')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+table.insert(opts, { desc = "Toggle Undo Tree" })
+keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>", opts)
