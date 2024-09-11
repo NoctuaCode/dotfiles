db49118717732268143d746d75af8a936cddbb53
NoctuaCode <noctuaapps@gmail.com>
2024-09-12 08:26:20 +1100

    feat: usage of astronvim config

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,45 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+
+  { import = "astrocommunity.completion.tabnine-nvim" },
+
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  { import = "astrocommunity.editing-support.dial-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.editing-support.nvim-treesitter-context" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.treesj" },
+  { import = "astrocommunity.editing-support.undotree" },
+
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+
+  { import = "astrocommunity.lsp.inc-rename-nvim" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.motion.mini-ai" },
+  { import = "astrocommunity.motion.mini-bracketed" },
+  { import = "astrocommunity.motion.nvim-surround" },
+
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.blade" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.laravel" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+
+  { import = "astrocommunity.test.neotest" },
+
+  { import = "astrocommunity.utility.noice-nvim" },
+}
