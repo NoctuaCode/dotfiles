e766d72004ec6fea25fa4209be58b6164c4314f7
NoctuaCode <noctuaapps@gmail.com>
2024-10-10 20:36:58 +1100

    feat(wezterm): Update to tokyonight, to FiraCode, increase font size to 20

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -19,13 +19,13 @@ return {
 	},
 	window_close_confirmation = "NeverPrompt",
 
-	color_scheme = "cyberdream",
+	color_scheme = "tokyonight",
 	-- term = "xterm-256color",
 	enable_tab_bar = false,
 	window_background_opacity = 0.8,
 
-	font_size = 18,
-	font = wezterm.font({ family = "DankMono Nerd Font" }),
+	font_size = 20,
+	font = wezterm.font({ family = "FiraCode Nerd Font" }),
 	bold_brightens_ansi_colors = true,
 	adjust_window_size_when_changing_font_size = true,
 
@@ -33,17 +33,17 @@ return {
 		{
 			intensity = "Bold",
 			italic = true,
-			font = wezterm.font({ family = "DankMono Nerd Font", weight = "Bold", style = "Italic" }),
+			font = wezterm.font({ family = "FiraCode Nerd Font", weight = "Bold", style = "Italic" }),
 		},
 		{
 			italic = true,
 			intensity = "Half",
-			font = wezterm.font({ family = "DankMono Nerd Font", weight = "DemiBold", style = "Italic" }),
+			font = wezterm.font({ family = "FiraCode Nerd Font", weight = "DemiBold", style = "Italic" }),
 		},
 		{
 			italic = true,
 			intensity = "Normal",
-			font = wezterm.font({ family = "DankMono Nerd Font", style = "Italic" }),
+			font = wezterm.font({ family = "FiraCode Nerd Font", style = "Italic" }),
 		},
 	},
 
