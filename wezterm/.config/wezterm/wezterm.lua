1aa12870aa4083a40f595e1640f7c0bc85ff26b8
NoctuaCode <noctuaapps@gmail.com>
2024-10-13 19:52:39 +1100

    feat(wezterm): Adding bindings for multiplexing

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,11 +6,50 @@ wezterm.on("gui-startup", function()
 	window:gui_window():maximize()
 end)
 
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 return {
 	front_end = "WebGpu",
 	webgpu_power_preference = "HighPerformance",
 
 	window_decorations = "RESIZE",
+	tab_bar_at_bottom = true,
 	window_padding = {
 		left = 10,
 		right = 10,
@@ -21,13 +60,11 @@ return {
 
 	color_scheme = "tokyonight",
 	-- term = "xterm-256color",
-	enable_tab_bar = false,
+	enable_tab_bar = true,
 	window_background_opacity = 0.8,
 
 	font_size = 20,
 	font = wezterm.font({ family = "FiraCode Nerd Font" }),
-	bold_brightens_ansi_colors = true,
-	adjust_window_size_when_changing_font_size = true,
 
 	font_rules = {
 		{
@@ -49,4 +86,57 @@ return {
 
 	use_dead_keys = false,
 	scrollback_lines = 10000,
+
+	leader = { key = "b", mods = "CTRL" },
+	keys = {
+		{
+			mods = "LEADER",
+			key = "|",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			mods = "LEADER",
+			key = "-",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			mods = "LEADER",
+			key = "d",
+			action = wezterm.action.CloseCurrentPane({ confirm = false }),
+		},
+		{
+			mods = "LEADER",
+			key = "m",
+			action = wezterm.action.TogglePaneZoomState,
+		},
+		-- rotate panes
+		{
+			mods = "LEADER",
+			key = "r",
+			action = wezterm.action.RotatePanes("Clockwise"),
+		},
+		-- show the pane selection mode, but have it swap the active and selected panes
+		{
+			mods = "LEADER",
+			key = "0",
+			action = wezterm.action.PaneSelect({
+				mode = "SwapWithActive",
+			}),
+		},
+		{
+			mods = "LEADER",
+			key = "Enter",
+			action = wezterm.action.ActivateCopyMode,
+		},
+		-- move between split panes
+		split_nav("move", "h"),
+		split_nav("move", "j"),
+		split_nav("move", "k"),
+		split_nav("move", "l"),
+		-- resize panes
+		split_nav("resize", "h"),
+		split_nav("resize", "j"),
+		split_nav("resize", "k"),
+		split_nav("resize", "l"),
+	},
 }
