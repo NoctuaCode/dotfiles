b756a82754ecc642f84b0e9b05b4fc241cad736b
Noctu'Apps <noctuaapps@gmail.com>
2025-03-13 23:53:53 +1100

    feat(fish): Update theme and prompt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,6 @@
 set fish_greeting ""
 set -gx TERM xterm-ghostty
 
-set -g theme_color_scheme terminal-dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
-
 set -gx EDITOR nvim
 
 set -g GOPATH $HOME/go
