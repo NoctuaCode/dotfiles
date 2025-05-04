cf616bb8e0efd737cdd5504d2461d6899ec7a047
Noctu'Apps <noctuaapps@gmail.com>
2025-05-04 17:43:22 +1100

    feat: Add sketchybar

--- config/sketchybar/items/init.lua
+++ config/sketchybar/items/init.lua
@@ -1,7 +1,12 @@
+-- Left items
 require("items.apple")
+require("items.menu_spaces_toggle")
 require("items.menus")
--- require("items.spaces")
-require("items.front_app")
-require("items.calendar")
+require("items.spaces")
+-- require("items.front_apps")
+
+-- Right items
+require("items.message")
 require("items.widgets")
-require("items.media")
+
+-- require("items.media")
