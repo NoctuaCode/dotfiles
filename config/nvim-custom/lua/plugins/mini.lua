2cb040dc23b64ebb0844ec1182149b91a1ef5285
NoctuaCode <noctuaapps@gmail.com>
2025-07-19 21:10:11 +1100

    feat: backup custom nvim config

---
+++ config/nvim-custom/lua/plugins/mini.lua
@@ -0,0 +1,33 @@
+return {
+	"echasnovski/mini.nvim",
+	config = function()
+		require("mini.ai").setup({ n_lines = 500 })
+		require("mini.surround").setup( -- No need to copy this inside `setup()`. Will be used automatically.
+			{
+				mappings = {
+					add = "gsa", -- Add surrounding in Normal and Visual modes
+					delete = "gsd", -- Delete surrounding
+					find = "gsf", -- Find surrounding (to the right)
+					find_left = "gsF", -- Find surrounding (to the left)
+					highlight = "gsh", -- Highlight surrounding
+					replace = "gsr", -- Replace surrounding
+					update_n_lines = "gsn", -- Update `n_lines`
+
+					suffix_last = "l", -- Suffix to search with "prev" method
+					suffix_next = "n", -- Suffix to search with "next" method
+				},
+			}
+		)
+		require("mini.comment").setup()
+		require("mini.splitjoin").setup()
+		require("mini.pairs").setup()
+
+		local statusline = require("mini.statusline")
+		statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+		---@diagnostic disable-next-line: duplicate-set-field
+		statusline.section_location = function()
+			return "%2l:%-2v"
+		end
+	end,
+}
