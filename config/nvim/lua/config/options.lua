6b4aeeb1df2c389c6110d7a84b242c4a1c524cce
NoctuaCode <noctuaapps@gmail.com>
2025-08-04 21:25:05 +1100

    feat: tokyonight in nvim, adding mise

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,3 +2,5 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim/undodir"
 vim.opt.undofile = true
+
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
