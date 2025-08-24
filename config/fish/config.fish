831c12dd65cd1a75c04b522075b06b2a2cdc60ef
NoctuaCode <noctuaapps@gmail.com>
2025-08-24 19:04:15 +1100

    feat: adding ops software completion and config into fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -43,3 +43,7 @@ end
 set -gx PATH $PATH /Users/caulotte/.lmstudio/bin
 # End of LM Studio CLI section
 
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
