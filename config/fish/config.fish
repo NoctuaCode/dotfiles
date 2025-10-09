dbd87b70f97a1b00e1bcd03dfe58df0d204d0020
NoctuaCode <noctuaapps@gmail.com>
2025-10-09 22:08:00 +1100

    cleanup fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -12,10 +12,6 @@ if status is-interactive
     set -x BUN_INSTALL $HOME/.bun
     set PATH $PATH $BUN_INSTALL/bin
 
-    # Carapace
-    set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
-    carapace _carapace | source
-
     # FZF
     fzf --fish | source
 
@@ -35,13 +31,3 @@ if status is-interactive
 
     source $HOME/.config/fish/config/aliases.fish
 end
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/caulotte/.lmstudio/bin
-# End of LM Studio CLI section
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-string match -q "$TERM_PROGRAM" kiro and . (kiro --locate-shell-integration-path fish)
