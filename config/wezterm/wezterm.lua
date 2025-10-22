2a50e01bb73a80acd0fe3327e92a1620e9847cde
NoctuaCode <noctuaapps@gmail.com>
2025-10-23 07:05:27 +1100

    change themes and font in wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "tokyonight_night"
-config.font_size = 14
-config.font = wezterm.font("GeistMono Nerd Font Mono")
+config.color_scheme = "nordfox"
+config.font_size = 16
+config.font = wezterm.font("Terminess Nerd Font")
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
 	left = 10,
