debb04a5d529223c039aa106acd76dfe6e13dd94
NoctuaCode <noctuaapps@gmail.com>
2026-02-05 16:49:22 -0500

    adding rust to nvim

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -3,4 +3,4 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim/undodir"
 vim.opt.undofile = true
 
-vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+-- vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
